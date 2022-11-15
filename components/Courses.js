a4dc7a8f214f272f4fec9463e0248caf3945d19f
Federico Freiberger <frei.federico@gmail.com>
2022-11-14 21:01:09 -0300

    courses advances

--- components/Courses.js
+++ components/Courses.js
@@ -1,10 +1,12 @@
 import { data } from "../coursesData";
 import { format } from "date-fns";
+import { useState } from "react";
 
-const CoursesTaken = () =>
+const CoursesTaken = ({ show }) =>
 	data
 		.filter((course) => course.dateCompleted)
 		.sort((course1, course2) => (course1.dateCompleted < course2.dateCompleted ? 1 : -1))
+		.slice(0, show)
 		.map((course) => (
 			<div className="col-md-4" key={course.id}>
 				<div className="work-box">
@@ -31,23 +33,42 @@ const CoursesTaken = () =>
 			</div>
 		));
 
-const Courses = () => (
-	<section id="courses" className="portfolio-mf sect-pt4 route">
-		<div className="container">
-			<div className="row">
-				<div className="col-sm-12">
-					<div className="title-box text-center">
-						<h3 className="title-a">Courses taken</h3>
-						<p className="subtitle-a">I'm always learning something.</p>
-						<div className="line-mf"></div>
-					</div>
-				</div>
-			</div>
-			<div className="row">
-				<CoursesTaken />
+const ShowMoreButton = ({ coursesToShow, setCoursesToShow }) => {
+	if (data.length <= coursesToShow) return null;
+
+	return (
+		<div className="row">
+			<div className="text-center">
+				<button className="btn btn-dark w-50" onClick={() => setCoursesToShow(coursesToShow + 9)}>
+					Show me more
+				</button>
 			</div>
 		</div>
-	</section>
-);
+	);
+};
+
+const Courses = () => {
+	const [coursesToShow, setCoursesToShow] = useState(12);
+
+	return (
+		<section id="courses" className="portfolio-mf sect-pt4 route">
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-12">
+						<div className="title-box text-center">
+							<h3 className="title-a">Courses taken</h3>
+							<p className="subtitle-a">I'm always learning something.</p>
+							<div className="line-mf"></div>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<CoursesTaken show={coursesToShow} />
+				</div>
+				<ShowMoreButton coursesToShow={coursesToShow} setCoursesToShow={setCoursesToShow} />
+			</div>
+		</section>
+	);
+};
 
 export default Courses;
